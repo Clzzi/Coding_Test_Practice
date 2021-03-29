035d2c01fc4c25806db17ee31667e215e70d0d86
Clzzi <sonmin1407@dgsw.hs.kr>
2021-03-29 14:50:00 +0900

    [LV.1] - 2016년

--- 2016년.js
+++ 2016년.js
@@ -1,6 +1,5 @@
 // .getDay()를 이용한 풀이
 function solution(a, b) {
-    const days = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
     const answer = days[new Date(`2016.${a}.${b}`).getDay()];
     return answer;
 }
